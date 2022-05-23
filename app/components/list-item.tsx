1acc28e8dd5b2901d9d30cf295b1b0f3df0ddff3
Evan Kerik <ekerik220@gmail.com>
2022-05-23 15:57:01 +0900

    fix custom markdown renderer

--- app/components/list-item.tsx
+++
@@ -1,9 +0,0 @@
-import { CFC } from "~/types/react-types"
-
-type Props = {
-  children: string
-}
-
-export const ListItem: CFC<Props> = ({ children }) => {
-  return <li className="mt-2 text-xl">{children}</li>
-}
